67befb77624a92135684984da42c886bcbfba054
ahbiee <james35744@gmail.com>
2025-11-18 23:27:36 +0800

    251118

--- LeetCode/315 Count of Smaller Numbers After Self.cpp
+++ LeetCode/315 Count of Smaller Numbers After Self.cpp
@@ -1,16 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-給定一個陣列，題目要求返回在所有index處，他右側比他小的元素
-如[3, 4, 9, 6, 1]，回傳陣列[1, 1, 2, 1, 0]
-*/
+// 一個更好的解法是Fenwick Tree (Binary Indexed Tree BIT)，這邊用歸併分治，有機會回來再試試看
+class Solution {
+public:
+    vector<int> arr, help, ans, idx, help_idx;
 
-vector<int> arr, help, ans, idx, help_idx;
+    vector<int> countSmaller(vector<int>& nums) {
+        int size = nums.size();
 
-void solve(int l, int m, int r);
+        arr.assign(size, 0);
+        help.assign(size, 0);
+        ans.assign(size, 0);
+        idx.assign(size, 0);
+        help_idx.assign(size, 0); 
 
-void merge(int l, int r){
+        for(int i=0; i<size; ++i){
+            arr[i] = nums[i];
+            idx[i] = i;
+        }
+        merge(0, size-1);
+
+        return ans;
+    }
+
+    void merge(int l, int r){
     if (l >= r) return; // l shouldn't be greater than r, and return when l == r
     
     int m = l + (r-l)/2; // prevent overflow
@@ -55,33 +69,4 @@ void solve(int l, int m, int r){
         idx[k] = help_idx[k];
     }
 }
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n; // the size of array
-
-    while(cin >> n){
-        arr.assign(n, 0);
-        help.assign(n, 0);
-        ans.assign(n, 0);
-        idx.assign(n, 0); // idx[i] holds the original index of i (which is the value of idx[i])
-        help_idx.assign(n, 0); // act like arr to help, idx to help_idx
-        
-        for(int i=0; i<n; ++i){
-            cin >> arr[i]; // get original array from input
-            idx[i] = i; // the original index of i is itself
-        }
-
-        // applying mergeSort, divide and conquer
-        merge(0, n-1);
-
-        for(int i=0; i<n; ++i){
-            cout << ans[i] << (i == n-1 ? '\n' : ' ');
-        }
-    }
-    
-
-    return 0;
-}
+};
